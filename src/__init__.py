0c42a2cd759b2ec733ae980cf448cf7878b6ceb1
Ian Hellen <ianhelle@microsoft.com>
2023-09-27 18:15:40 -0700

    Updated classes and test notebooks

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""Module docstring."""
+
+from ._version import VERSION
+
+__version__ = VERSION
+__author__ = "Ian Hellen"
